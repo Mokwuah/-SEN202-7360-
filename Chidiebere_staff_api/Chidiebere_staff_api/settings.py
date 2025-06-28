3e98c6b03e969f57649837a2a4d6cf6a47e17af1
Mokwuah <chidieberemokwuah@gmail.com>
2025-06-28 11:28:00 +0100

    final commit

--- Chidiebere_staff_api/Chidiebere_staff_api/settings.py
+++ Chidiebere_staff_api/Chidiebere_staff_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employees python manage.py createsuperuser'
+    'employees'
 ]
 
 MIDDLEWARE = [
